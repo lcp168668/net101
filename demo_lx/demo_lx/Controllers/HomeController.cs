7a24098f303bade5252f9e7739af775c3d190cd8
lcp <lcp2018668@163.com>
2018-03-16 11:39:36 +0800

    add

---
+++ demo_lx/demo_lx/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo_lx.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //这是一个添加方法
+        public ActionResult Add() 
+        {
+            return View();
+        }
+    }
+}
---
+++ demo_lx/demo_lx/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="demo_lx.MvcApplication" Language="C#" %>
